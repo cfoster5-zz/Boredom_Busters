02f4f9a6740f4f0c6e7d0807a9d83fd017f0a4a2
dgtang <dgtang@eagles.usi.edu>
2017-07-11 13:56:03 -0500

    vendor page

--- src/pages/vendorpage/vendorpage.ts
+++ src/pages/vendorpage/vendorpage.ts
@@ -2,37 +2,14 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-vendorlist',
-  templateUrl: 'vendorlist.html'
+  selector: 'page-vendorpage',
+  templateUrl: 'vendorpage.html'
 })
-export class VendorListPage {
+export class VendorPagePage {
 
   constructor(public navCtrl: NavController) {
 
   }
 
-  items = [
-  'Pokémon Yellow',
-  'Super Metroid',
-  'Mega Man X',
-  'The Legend of Zelda',
-  'Pac-Man',
-  'Super Mario World',
-  'Street Fighter II',
-  'Half Life',
-  'Final Fantasy VII',
-  'Star Fox',
-  'Tetris',
-  'Donkey Kong III',
-  'GoldenEye 007',
-  'Doom',
-  'Fallout',
-  'GTA',
-  'Halo'
-];
-
-itemSelected(item: string) {
-  console.log("Selected Item", item);
-}
 
 }
