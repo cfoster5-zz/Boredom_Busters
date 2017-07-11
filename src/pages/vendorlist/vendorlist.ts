fc79b2e470efce25504dc28b9e100148c55e55ce
cfoster5 <Corey@Coreys-MacBook-Pro.local>
2017-07-11 10:06:09 -0500

    Added dummy data to Vendor page

--- src/pages/vendorlist/vendorlist.ts
+++ src/pages/vendorlist/vendorlist.ts
@@ -11,4 +11,28 @@ export class VendorListPage {
 
   }
 
+  items = [
+  'Pokémon Yellow',
+  'Super Metroid',
+  'Mega Man X',
+  'The Legend of Zelda',
+  'Pac-Man',
+  'Super Mario World',
+  'Street Fighter II',
+  'Half Life',
+  'Final Fantasy VII',
+  'Star Fox',
+  'Tetris',
+  'Donkey Kong III',
+  'GoldenEye 007',
+  'Doom',
+  'Fallout',
+  'GTA',
+  'Halo'
+];
+
+itemSelected(item: string) {
+  console.log("Selected Item", item);
+}
+
 }
