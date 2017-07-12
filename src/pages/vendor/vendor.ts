5734625902c4946a2b390cf7d79eada81b1c818b
dgtang <dgtang@eagles.usi.edu>
2017-07-12 10:14:57 -0500

    pulling vendor data

--- src/pages/vendor/vendor.ts
+++ src/pages/vendor/vendor.ts
@@ -7,11 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class VendorPage {
 
+  items = [];
   constructor(public navCtrl: NavController) {
 
-
+  fetch('http://34.210.2.173/Vendors.php')
+   .then((response) => {
+       if (response.status !== 200) {
+         console.log('Looks like there was a problem. Status Code: ' +
+           response.status);
+         return;
+       }
+       // Examine the text in the response
+       response.json().then((data) => {
+         console.log(data);
+         document.getElementById("vendor1").innerHTML = data[0].VEN_Name;
+         //this.items = data;
+       });
+     }
+   )
+   .catch(function(err) {
+     console.log('Fetch Error :-S', err);
+   });
 
   }
+
+
+
 
 
 }
