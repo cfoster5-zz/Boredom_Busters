af6c64a663112dce8088e320d15f7ef8ae3edae8
cfoster5 <Corey@Coreys-MacBook-Pro.local>
2017-07-12 11:23:57 -0500

    Pulling data for Coupons page

--- src/pages/coupons/coupons.ts
+++ src/pages/coupons/coupons.ts
@@ -9,9 +9,50 @@ import { SearchPage } from '../search/search';
 })
 export class CouponsPage {
 
-  constructor(public navCtrl: NavController) {
+constructor(public navCtrl: NavController) {
 
-  }
+ fetch('http://34.210.2.173/Vendors.php')
+  .then((response) => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      // Examine the text in the response
+      response.json().then((data) => {
+        console.log(data);
+        document.getElementById("VENName").innerHTML = data[0].VEN_Name;
+        document.getElementById("VENIMG").innerHTML = data[0].VEN_Img;
+
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+
+  fetch('http://34.210.2.173/Coupons.php')
+   .then((response) => {
+       if (response.status !== 200) {
+         console.log('Looks like there was a problem. Status Code: ' +
+           response.status);
+         return;
+       }
+       // Examine the text in the response
+       response.json().then((data) => {
+         console.log(data);
+         document.getElementById("CPNDesc").innerHTML = data[0].CPN_Desc;
+
+       });
+     }
+   )
+   .catch(function(err) {
+     console.log('Fetch Error :-S', err);
+   });
+
+
+}
 
   showsearch() {
       this.navCtrl.push(SearchPage);
