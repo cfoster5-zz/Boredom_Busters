91674a5cb18dfada8c048736e86aaabe302d0ae6
cfoster5 <Corey@Coreys-MacBook-Pro.local>
2017-07-12 15:04:26 -0500

    Updates to Search page
    Pulling data for search bar. Can now go to Vendors page through search

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { VendorPage } from '../vendor/vendor';
+
 
 @Component({
   selector: 'page-search',
@@ -11,55 +13,33 @@ export class SearchPage {
 
   constructor(public navCtrl: NavController) {
 
-    this.initializeItems();
+  fetch('http://34.210.2.173/Vendors.php')
+   .then((response) => {
+       if (response.status !== 200) {
+         console.log('Looks like there was a problem. Status Code: ' +
+           response.status);
+         return;
+       }
+       // Examine the text in the response
+       response.json().then((data) => {
+         console.log(data);
+        // document.getElementById().innerHTML = data[0].VEN_Name;
+         this.items = data;
+
+       });
+     }
+   )
+   .catch(function(err) {
+     console.log('Fetch Error :-S', err);
+   });
+
 
   }
 
-    initializeItems() {
-      this.items = [
-        'Amsterdam',
-        'Bogota',
-        'Buenos Aires',
-        'Cairo',
-        'Dhaka',
-        'Edinburgh',
-        'Geneva',
-        'Genoa',
-        'Glasglow',
-        'Hanoi',
-        'Hong Kong',
-        'Islamabad',
-        'Istanbul',
-        'Jakarta',
-        'Kiel',
-        'Kyoto',
-        'Le Havre',
-        'Lebanon',
-        'Lhasa',
-        'Lima',
-        'London',
-        'Los Angeles',
-        'Madrid',
-        'Manila',
-        'New York',
-        'Olympia',
-        'Oslo',
-        'Panama City',
-        'Peking',
-        'Philadelphia',
-        'San Francisco',
-        'Seoul',
-        'Taipeh',
-        'Tel Aviv',
-        'Tokio',
-        'Uelzen',
-        'Washington'
-      ];
-    }
 
     getItems(ev) {
       // Reset items back to all of the items
-      this.initializeItems();
+      this.items();
 
       // set val to the value of the ev target
       var val = ev.target.value;
@@ -71,5 +51,11 @@ export class SearchPage {
         })
       }
     }
+
+
+    selectvendor() {
+        this.navCtrl.push(VendorPage);
+    }
+
 
 }
