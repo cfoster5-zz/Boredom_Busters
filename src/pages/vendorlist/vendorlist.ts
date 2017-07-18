eddc40858a018069fda428fbe671dfbb3df3f472
dgtang <dgtang@eagles.usi.edu>
2017-07-18 14:56:06 -0500

    trying to connect submit to vendor list

--- src/pages/vendorlist/vendorlist.ts
+++ src/pages/vendorlist/vendorlist.ts
@@ -10,11 +10,13 @@ import { VendorPage } from '../vendor/vendor';
 })
 export class VendorListPage {
   SUBCAT_NAME;
+  SUBCAT_ID;
+  SUBCAT_MASTER;
   items = [];
    constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.SUBCAT_NAME =this.navParams.get("SUBCAT_NAME");
     console.log("VendorListPage",this.navParams)//url?name=value&name=value&...
-   fetch('http://34.210.2.173/Vendors.php')
+   fetch('http://34.210.2.173/Vendors.php?SUBCAT_ID='+this.navParams.get('SUBCAT_MASTER'))
     .then((response) => {
         if (response.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' +
@@ -24,25 +26,25 @@ export class VendorListPage {
         // Examine the text in the response
         response.json().then((data) => {
           //console.log(data);
-        
+
           this.items = data;
-          
+
         });
       }
     )
     .catch(function(err) {
       console.log('Fetch Error :-S', err);
-    
+
     });
 
 
-  } 
+  }
    ionViewDidLoad(){
-     
-   
+
+
    console.log(this.navParams.get("SUBCAT_NAME"));
-  
-  } 
+
+  }
 
 selectvendor(item) {
   console.log("Selected Item", item);
